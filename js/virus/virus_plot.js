7f7b29e32952186de1f5fa8d524bf5b126fb5849
AlanSPAGHETTI <Alanlongsecure@protonmail.com>
2020-03-22 23:19:29 -0700

    Update virus_plot.js

--- js/virus/virus_plot.js
+++ js/virus/virus_plot.js
@@ -7,6 +7,7 @@ class VirusPlot extends Chart
 
         vis.selected_country_name_1 = "Mainland China";
         vis.selected_country_name_2 = "Thailand";
+        vis.number_of_days          = 5;
 
         // Promise chaining: dataset has its own initialize() method we wait for
         vis.config.dataset.initialize().then(dataset =>
