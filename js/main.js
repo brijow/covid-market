053b8d7885238a76582e9e95f2663599bccc1ef7
Brian White <brianwhite@Brians-MacBook-Air.local>
2020-03-28 19:05:13 -0700

    cleanup comments in main.js

--- js/main.js
+++ js/main.js
@@ -1,84 +1,26 @@
-//////////////////////
-/// CHOROPLETH MAP ///
-//////////////////////
+/*
+ * State management
+ * ----------------
+ * We will use this state variable in order to keep track of things for bidirectional
+ * interactivity.  These include start/end dates and the selected countries.
+ *
+ * Note for developers: The three things that you will need to access in your update()
+ * method are:
+ *   - state.startDate
+ *   - state.endDate
+ *   - state.selectedCountries
+ * NEVER MODIFY THESE PROPERTIES WITHOUT CALLING THE SETTERS!  The setters will take care
+ * of error checking and calling the update() method of all the charts.
+ */
+const DATE_START = new Date('01/22/2020'); // min date common to all our datasets
+const DATE_END = new Date('03/11/20');     // max date common to all our datasets
+const MAX_COUNTRIES = 4;                   // select maximum of 4 countries at a time
 
-// Instantiate map dataset (data not loaded until we call initVis())
-let mapData = new MapData({
-  fileNames: ['data/covid_19_data.csv',
-              'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json',]
-});
-let map = new Map({
-  parentElement: '#map',
-  dataset : mapData,
-  containerWidth: "100%",
-  containerHeight: "100%"
-});
-
-//////////////////////
-/// LINE+BAR CHART ///
-//////////////////////
-
-// Instantiate time series dataset (data not loaded until we call initVis())
-let virusData = new VirusData({
-  fileNames: ['data/time_series_covid_19_confirmed.csv',
-              'data/time_series_covid_19_deaths.csv',
-              'data/time_series_covid_19_recovered.csv',]
-});
-
-// Instantiate virus plot (data not loaded until we call initVis())
-let virus = new VirusPlot({
-  parentElement: '#virus_plot',
-  dataset : virusData,
-  containerWidth: "100%",
-  containerHeight: "100%"
-});
-
-//////////////////////
-//// STOCKS CHART ////
-//////////////////////
-
-// Instantiate map dataset (data not loaded until we call initVis())
-let stocksData = new StocksData({
-  fileNames: ['data/s_p_500.csv',]
-});
-// Instantiate stocks plot (data not loaded until we call initVis())
-let stocks = new StocksPlot({
-  parentElement: '#stocks_plot',
-  dataset : stocksData,
-  containerWidth: 1200,
-  containerHeight: 200
-});
-
-//////////////////////
-// STATE MANAGEMENT //
-//////////////////////
-
-// We will use this state variable in order to keep track of things for bidirectional
-// interactivity.  These include start/end dates and the selected countries.
-//
-// Note for developers: The three things that you will need to access in your update()
-// method are:
-//   - state.startDate
-//   - state.endDate
-//   - state.selectedCountries
-// NEVER MODIFY THESE PROPERTIES WITHOUT CALLING THE SETTERS!  The setters will take care
-// of error checking and calling the update() method of all the charts.
-
-// First: set constants and defaults
-// Set the default start and end dates to the date ranges in our COVID-19 data
-// Set selected countries to be empty, with a limit on 4 countries at a time
-const DATE_START = new Date('01/22/2020');
-const DATE_END = new Date('03/11/20');
-const MAX_COUNTRIES = 4;
-
-// Next, declare state
 const state = {
-  // Variables
   startDate: DATE_START,
   endDate: DATE_END,
   selectedCountries: [],
 
-  // Setters
   setStartDate: function(date) {
     if (date < this.endDate && date >= DATE_START) {
       this.startDate = date;
@@ -124,13 +66,66 @@ const state = {
     stocks.update();
   }
 };
-console.log(state);
+//console.log(state);
 
-// This does some Promise chaining:
-// 1. Calls initVis() on dataset attribute (a Dataset object)
-//    which returns a promise.
-// 2. When that promise is resolved, dataset has been fully initialized,
-//    and so we can finish initialization of choroplethMap.
+/*
+ * Instantiate Dataset objects
+ * ---------------------------
+ */
+const mapData = new MapData({
+  fileNames: ['data/covid_19_data.csv',
+              'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-50m.json',]
+});
+const virusData = new VirusData({
+  fileNames: ['data/time_series_covid_19_confirmed.csv',
+              'data/time_series_covid_19_deaths.csv',
+              'data/time_series_covid_19_recovered.csv',]
+});
+const stocksData = new StocksData({
+  fileNames: ['data/s_p_500.csv',
+              'data/dow_jones.csv',]
+});
+
+/*
+ * Instantiate Chart objects
+ * ---------------------------
+ * 1. Choropleth map
+ *   - visualize covid-19 statistics per country within a variable date range
+ *   - state.startDate and state.endDate are used for the date range
+ *
+ * 2. Grouped bar chart
+ *   - compare covid-19 statistics between countries within a variable date range
+ *   - state.startDate and state.endDate are used for the date range
+ *
+ * 3. Multi-line chart
+ *   - visualize stock market changes over a fixed date range
+ *   - DATE_START and DATE_END are used for the (fixed) date range
+ *   - a brush (will be) used to set state.startDate and state.endDate (TODO)
+ */
+const map = new Map({
+  parentElement: '#map',
+  dataset : mapData,
+  containerWidth: "100%",
+  containerHeight: "100%"
+});
+const virus = new VirusPlot({
+  parentElement: '#virus_plot',
+  dataset : virusData,
+  containerWidth: "100%",
+  containerHeight: "100%"
+});
+const stocks = new StocksPlot({
+  parentElement: '#stocks_plot',
+  dataset : stocksData,
+  containerWidth: 1200,
+  containerHeight: 200
+});
+
+/*
+ * Initialize and render all the charts
+ * ------------------------------------
+ * NOTE: each <Chart>.initvis() involves some Promise chaining.
+ */
 map.initVis();
 virus.initVis();
 stocks.initVis();
