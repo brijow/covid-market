ea141631242234e3fcd58847f63583790b796cbe
AlanSPAGHETTI <Alanlongsecure@protonmail.com>
2020-04-03 23:08:19 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -130,3 +130,38 @@ const stocks = new StocksPlot({
 map.initVis();
 virus.initVis();
 stocks.initVis();
+
+/*
+ * The following code is for Alan's chart
+ */
+$(document).ready(function()
+{
+    $("#form_virus_display").change(function()
+    {
+        selected_value = $("input[name='name_virus_display']:checked").val();
+
+        if (selected_value === "confirmed")
+        {
+            virus.visualize_confirmed = true;
+            virus.visualize_dead      = false;
+            virus.visualize_recovered = false;
+            virus.update();
+        }
+
+        if (selected_value === "dead")
+        {
+            virus.visualize_confirmed = false;
+            virus.visualize_dead      = true;
+            virus.visualize_recovered = false;
+            virus.update();
+        }
+
+        if (selected_value === "recovered")
+        {
+            virus.visualize_confirmed = false;
+            virus.visualize_dead      = false;
+            virus.visualize_recovered = true;
+            virus.update();
+        }
+    });
+});
