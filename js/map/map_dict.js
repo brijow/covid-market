83c023f4f2528aea86efa8ff2d095a484cdfaff3
AlanSPAGHETTI <Alanlongsecure@protonmail.com>
2020-04-06 19:34:24 -0700

    Update map_dict.js

--- js/map/map_dict.js
+++ js/map/map_dict.js
@@ -3,7 +3,7 @@ const MapDict = {
  * We have 26 countries in our data file that have discrepancies with our topojson:
 
   -----------------------------------------------------------------------------
-  |        COVID-19 Data File          |             TopoJSON                 |  
+  |        COVID-19 Data File          |             TopoJSON                 |
   -------------------------------- Easy cases ---------------------------------
   | Mainland China                     | China
   | Macau                              | Macao
@@ -36,27 +36,27 @@ const MapDict = {
   country names and our TopoJSON file country names.  There are some exceptions that
   go beyond simple lookup that will be dealt with separately.
  */
- 	'Mainland China': 'China',
- 	'Macau': 'Macao',
- 	'US': 'United States of America',
- 	'Ivory Coast': "Côte d'Ivoire",
- 	'UK': 'United Kingdom',
- 	'North Macedonia': 'Macedonia',
- 	'Czech Republic': 'Czechia',
- 	'Dominican Republic': 'Dominican Rep.',
- 	'Saint Barthelemy': 'St-Barthélemy',
- 	'Faroe Islands': 'Faeroe Is.',
- 	'Bosnia and Herzegovina': 'Bosnia and Herz.',
- 	'Vatican City': 'Vatican',
- 	'Republic of Ireland': 'Ireland',
- 	'St. Martin': 'St-Martin',
- 	'occupied Palestinian territory': 'Palestine',
- 	"('St. Martin',)": 'St-Martin',
- 	'Congo (Kinshasa)': 'Dem. Rep. Congo',
- 	'Gibraltar': 'United Kingdom',
- 	'French Guiana': 'France',
- 	'Holy See': 'Vatican',
- 	'Martinique': 'France',
- 	'Reunion': 'France',
- 	'North Ireland': 'United Kingdom'
+ 	'Mainland China'                 : 'China',
+ 	'Macau'                          : 'Macao',
+ 	'US'                             : 'United States of America',
+ 	'Ivory Coast'                    : "Côte d'Ivoire",
+ 	'UK'                             : 'United Kingdom',
+ 	'North Macedonia'                : 'Macedonia',
+ 	'Czech Republic'                 : 'Czechia',
+ 	'Dominican Republic'             : 'Dominican Rep.',
+ 	'Saint Barthelemy'               : 'St-Barthélemy',
+ 	'Faroe Islands'                  : 'Faeroe Is.',
+ 	'Bosnia and Herzegovina'         : 'Bosnia and Herz.',
+ 	'Vatican City'                   : 'Vatican',
+ 	'Republic of Ireland'            : 'Ireland',
+ 	'St. Martin'                     : 'St-Martin',
+ 	'occupied Palestinian territory' : 'Palestine',
+ 	"('St. Martin',)"                : 'St-Martin',
+ 	'Congo (Kinshasa)'               : 'Dem. Rep. Congo',
+ 	'Gibraltar'                      : 'United Kingdom',
+ 	'French Guiana'                  : 'France',
+ 	'Holy See'                       : 'Vatican',
+ 	'Martinique'                     : 'France',
+ 	'Reunion'                        : 'France',
+ 	'North Ireland'                  : 'United Kingdom'
 };
